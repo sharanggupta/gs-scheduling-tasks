659e88211159c7f5d5ab40d398984a157e3ef894
sharanggupta <sharang.gupta@sharanggupta.dev>
2024-11-15 22:32:12 +0000

    Added scheduled task

--- src/main/java/dev/sharanggupta/gs_scheduling_tasks/GsSchedulingTasksApplication.java
+++ src/main/java/dev/sharanggupta/gs_scheduling_tasks/GsSchedulingTasksApplication.java
@@ -2,8 +2,10 @@ package dev.sharanggupta.gs_scheduling_tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GsSchedulingTasksApplication {
 
 	public static void main(String[] args) {
