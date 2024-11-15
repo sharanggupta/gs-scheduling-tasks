02a5412a146d91e63b04c482134eaa700d7f127c
sharanggupta <sharang.gupta@sharanggupta.dev>
2024-11-15 21:56:23 +0000

    Initial commit

---
+++ src/main/java/dev/sharanggupta/gs_scheduling_tasks/GsSchedulingTasksApplication.java
@@ -0,0 +1,13 @@
+package dev.sharanggupta.gs_scheduling_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSchedulingTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSchedulingTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sharanggupta/gs_scheduling_tasks/GsSchedulingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sharanggupta.gs_scheduling_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSchedulingTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
